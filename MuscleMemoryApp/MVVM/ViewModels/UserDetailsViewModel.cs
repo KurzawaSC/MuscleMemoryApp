32df81ed1de813ea92af8ed665166cc6943db956
Kacper Kurzawski <kurzawa.sc@gmail.com>
2024-07-25 15:27:35 +0200

    Registration process now works correctly

---
+++ MuscleMemoryApp/MVVM/ViewModels/UserDetailsViewModel.cs
@@ -0,0 +1,253 @@
+﻿using MuscleMemoryApp.MVVM.Models;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Text.Json;
+
+namespace MuscleMemoryApp.MVVM.ViewModels;
+
+public class UserDetailsViewModel
+{
+    public List<string> sexes { get; set; }
+    public List<string> nationalities { get; set; }
+    public RegisterRequest request { get; set; }
+    public User user { get; set; }
+
+    HttpClient client;
+    JsonSerializerOptions serializerOptions;
+    string baseUrl = "https://localhost:7002";
+    public string repeatedPassword { get; set; }
+
+    public UserDetailsViewModel()
+    {
+        sexes = new List<string>()
+        {
+            "male", "female"
+        };
+        nationalities = new List<string>()
+        {
+            "Afghan",
+            "Albanian",
+            "Algerian",
+            "American",
+            "Andorran",
+            "Angolan",
+            "Antiguans",
+            "Argentinean",
+            "Armenian",
+            "Australian",
+            "Austrian",
+            "Azerbaijani",
+            "Bahamian",
+            "Bahraini",
+            "Bangladeshi",
+            "Barbadian",
+            "Barbudans",
+            "Batswana",
+            "Belarusian",
+            "Belgian",
+            "Belizean",
+            "Beninese",
+            "Bhutanese",
+            "Bolivian",
+            "Bosnian",
+            "Brazilian",
+            "British",
+            "Bruneian",
+            "Bulgarian",
+            "Burkinabe",
+            "Burmese",
+            "Burundian",
+            "Cambodian",
+            "Cameroonian",
+            "Canadian",
+            "Cape Verdean",
+            "Central African",
+            "Chadian",
+            "Chilean",
+            "Chinese",
+            "Colombian",
+            "Comoran",
+            "Congolese",
+            "Costa Rican",
+            "Croatian",
+            "Cuban",
+            "Cypriot",
+            "Czech",
+            "Danish",
+            "Djibouti",
+            "Dominican",
+            "Dutch",
+            "East Timorese",
+            "Ecuadorean",
+            "Egyptian",
+            "Emirian",
+            "Equatorial Guinean",
+            "Eritrean",
+            "Estonian",
+            "Ethiopian",
+            "Fijian",
+            "Filipino",
+            "Finnish",
+            "French",
+            "Gabonese",
+            "Gambian",
+            "Georgian",
+            "German",
+            "Ghanaian",
+            "Greek",
+            "Grenadian",
+            "Guatemalan",
+            "Guinea-Bissauan",
+            "Guinean",
+            "Guyanese",
+            "Haitian",
+            "Herzegovinian",
+            "Honduran",
+            "Hungarian",
+            "I-Kiribati",
+            "Icelander",
+            "Indian",
+            "Indonesian",
+            "Iranian",
+            "Iraqi",
+            "Irish",
+            "Israeli",
+            "Italian",
+            "Ivorian",
+            "Jamaican",
+            "Japanese",
+            "Jordanian",
+            "Kazakhstani",
+            "Kenyan",
+            "Kittian and Nevisian",
+            "Kuwaiti",
+            "Kyrgyz",
+            "Laotian",
+            "Latvian",
+            "Lebanese",
+            "Liberian",
+            "Libyan",
+            "Liechtensteiner",
+            "Lithuanian",
+            "Luxembourger",
+            "Macedonian",
+            "Malagasy",
+            "Malawian",
+            "Malaysian",
+            "Maldivan",
+            "Malian",
+            "Maltese",
+            "Marshallese",
+            "Mauritanian",
+            "Mauritian",
+            "Mexican",
+            "Micronesian",
+            "Moldovan",
+            "Monacan",
+            "Mongolian",
+            "Moroccan",
+            "Mosotho",
+            "Motswana",
+            "Mozambican",
+            "Namibian",
+            "Nauruan",
+            "Nepalese",
+            "New Zealander",
+            "Ni-Vanuatu",
+            "Nicaraguan",
+            "Nigerian",
+            "Nigerien",
+            "North Korean",
+            "Northern Irish",
+            "Norwegian",
+            "Omani",
+            "Pakistani",
+            "Palauan",
+            "Panamanian",
+            "Papua New Guinean",
+            "Paraguayan",
+            "Peruvian",
+            "Polish",
+            "Portuguese",
+            "Qatari",
+            "Romanian",
+            "Russian",
+            "Rwandan",
+            "Saint Lucian",
+            "Salvadoran",
+            "Samoan",
+            "San Marinese",
+            "Sao Tomean",
+            "Saudi",
+            "Scottish",
+            "Senegalese",
+            "Serbian",
+            "Seychellois",
+            "Sierra Leonean",
+            "Singaporean",
+            "Slovakian",
+            "Slovenian",
+            "Solomon Islander",
+            "Somali",
+            "South African",
+            "South Korean",
+            "Spanish",
+            "Sri Lankan",
+            "Sudanese",
+            "Surinamer",
+            "Swazi",
+            "Swedish",
+            "Swiss",
+            "Syrian",
+            "Taiwanese",
+            "Tajik",
+            "Tanzanian",
+            "Thai",
+            "Togolese",
+            "Tongan",
+            "Trinidadian or Tobagonian",
+            "Tunisian",
+            "Turkish",
+            "Tuvaluan",
+            "Ugandan",
+            "Ukrainian",
+            "Uruguayan",
+            "Uzbekistani",
+            "Venezuelan",
+            "Vietnamese",
+            "Welsh",
+            "Yemenite",
+            "Zambian",
+            "Zimbabwean"
+        };
+        client = new HttpClient();
+        serializerOptions = new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+        };
+        request = new RegisterRequest();
+        user = new User();
+        repeatedPassword = string.Empty;
+    }
+
+    public async Task RegisterUser()
+    {
+        string json = JsonSerializer.Serialize(request, serializerOptions);
+
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+        var response = await client.PostAsync($"{baseUrl}/api/identity/register", content);
+    }
+
+    public async Task EditUserInfo(string token)
+    {
+        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        string json = JsonSerializer.Serialize(user, serializerOptions);
+
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await client.PatchAsync($"{baseUrl}/api/identity", content);
+    }
+}
--- MuscleMemoryApp/MVVM/Views/RegisterView.xaml.cs
+++ MuscleMemoryApp/MVVM/Views/RegisterView.xaml.cs
@@ -4,23 +4,43 @@ namespace MuscleMemoryApp.MVVM.Views;
 
 public partial class RegisterView : ContentPage
 {
-	RegisterViewModel _viewModel;
+	UserDetailsViewModel _viewModel;
 	public RegisterView()
 	{
 		InitializeComponent();
-		_viewModel = new RegisterViewModel();
+		_viewModel = new UserDetailsViewModel();
 		BindingContext = _viewModel;
 	}
 
     private async void Confirm_Clicked(object sender, EventArgs e)
     {
-		await _viewModel.RegisterUser();
-		//await _viewModel.editUserInfo.EditUserInfo();
-		await Navigation.PopAsync();
+		if(_viewModel.request.password == _viewModel.repeatedPassword)
+		{
+            await _viewModel.RegisterUser();
+            var login = new LoginPageViewModel()
+            {
+                eMail = _viewModel.request.email,
+                password = _viewModel.request.password
+            };
+            await login.LogIn();
+            await _viewModel.EditUserInfo(login.loginResponse.accessToken);
+            await Navigation.PopAsync();
+        }
+        else
+        {
+            await DisplayAlert("Passwords are diffrent", "Passwords must be equal", "Ok");
+        }
+		
     }
 
     private async void Cancel_Clicked(object sender, EventArgs e)
     {
         await Navigation.PopAsync();
+    }
+
+    private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+		DatePicker datePicker = (DatePicker)sender;
+		_viewModel.user.DateOfBirth = DateOnly.FromDateTime(datePicker.Date);
     }
 }
